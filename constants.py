ab4d6072e7176b8220712d2064344e6fdce1836c
Ali Furkan Budak <40214943+alifurkanbudak@users.noreply.github.com>
2021-01-08 17:49:25 +0300

    Added some communication functions

--- constants.py
+++ constants.py
@@ -1,10 +1,11 @@
+# PyGame constants =========================
 WHITE = (255, 255, 255)
 GREY = (70, 70, 70)
 BLACK = (0, 0, 0)
 FOOD_COLOR = (200, 0, 0)
 SNAKE_COLORS = [(0, 255, 0), (53, 98, 191), (155, 180, 0),
                 (155, 98, 0), (116, 98, 191), (18, 188, 210), (0, 100, 0)]
-
+# Directions
 LEFT = 0
 UP = 1
 RIGHT = 2
@@ -15,3 +16,20 @@ WINDOW_WIDTH = 750
 BLOCK_SIZE = 15
 GRID_HEIGHT = WINDOW_HEIGHT // BLOCK_SIZE
 GRID_WIDTH = WINDOW_WIDTH // BLOCK_SIZE
+
+# Communication constants ==================
+MY_IP = ''
+SERVER_IP = ''
+PORT = 12345
+BUFFER_SIZE = 1500
+
+MY_SNAKE_ID = ''
+
+# Client message types
+JOIN_REQUEST = 0
+MOVEMENT_REQUEST = 1
+
+# Server UDP message types
+SNAKE_SPAWN = 0
+FOOD_SPAWN = 1
+MOVEMENTS = 2
